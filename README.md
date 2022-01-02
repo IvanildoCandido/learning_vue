ad46cb57fdbcd7054fc4ec6b223efa53177f5682
Ivanildo Cândido Bezerra <systechtecnologia@gmail.com>
2022-01-02 17:40:36 -0300

    Loops e condicionais

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Repositório de aprendizagem da biblioteca Vue
 
 1- Olá mundo em Vue!
+2- Loops e Condicionais
--- script.js
+++ script.js
@@ -6,3 +6,18 @@ let name = new Vue({
   el: "#name",
   data: { name: "Ivanildo Cândido" },
 });
+let score = new Vue({
+  el: "#score",
+  data: { score: 5, info: "Parabéns você passou de 5 pontos!" },
+});
+let list = new Vue({
+  el: "#list",
+  data: {
+    people: [
+      { name: "Ivanildo", lastName: "Cândido", age: 34 },
+      { name: "Marizelma", lastName: "Aragão", age: 26 },
+      { name: "Yann", lastName: "Cândido", age: 9 },
+      { name: "Sammya", lastName: "Cândido", age: 11 },
+    ],
+  },
+});
